6afa6930a841a6db2b1a0d680f5c644c8c3f574a
ivanringelmann <iskimanr@gmail.com>
2022-10-19 16:06:45 +0500

    Решил первую задачу

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int InCenter(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
+
+Console.WriteLine(InCenter(num));
